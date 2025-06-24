d6c6737e5726cc62027fade10c7e5529d6dda070
Ayeni Bidemi <bidemia02@gmail.com>
2025-06-24 06:34:05 +0100

    blos and search features

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,11 @@
 from .models import Category
+from assignment.models import SocialLink
 
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+
+def get_scoial_links(request):
+    social_links = SocialLink.objects.all()
+    return dict(social_links=social_links)
